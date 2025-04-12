4fc60824f3eac7eb82d6df7a2e18110953d9432d
Ana Galván <ana.galvan123456@gmail.com>
2025-04-12 19:14:12 +0200

    added code for pipeline.log in pipeline.sh

--- scripts/pipeline.sh
+++ scripts/pipeline.sh
@@ -28,18 +28,32 @@ for file in out/merged/*.fastq.gz; do
     cutadapt -m 18 -a TGGAATTCTCGGGTGCCAAGG --discard-untrimmed \
     -o out/trimmed/${sample}.trimmed.fastq.gz "$file"\
     > log/cutadapt/${sample}.log #run cutadapt for each merged file and save the log
+    
+    # Include results from cutadapt from each sample to a common log fil
+    echo "Processing $sample with cutadapt" >> log/pipeline.log
+    grep "Reads with adapters" log/cutadapt/${sample}.log >> log/pipeline.log
+    grep "Total basepairs" log/cutadapt/${sample}.log >> log/pipeline.log
+    echo "----------------------------------------" >> log/pipeline.log
 done 
 
 # Run STAR for all trimmed files
 echo "Running STAR alignment..."
 for fname in out/trimmed/*.fastq.gz
 do 
-    obtain the sample ID from the filename
+    #obtain the sample ID from the filename
     sid=$(basename "$fname" .trimmed.fastq.gz)
     mkdir -p out/star/$sid
+    echo "Aligning file: $fname"
     STAR --runThreadN 4 --genomeDir res/contaminants_idx \
-    --outReadsUnmapped Fastx --readFilesIn out/trimmed/$fname\
+    --outReadsUnmapped Fastx --readFilesIn $fname\
     --readFilesCommand gunzip -c --outFileNamePrefix out/star/$sid/ 
+
+    # Include relevant results from STAR from each sample to a common log file
+    echo "Processing $sid with STAR" >> log/pipeline.log
+    grep "Uniquely mapped reads %" out/star/$sid/Log.final.out >> log/pipeline.log
+    grep "% of reads mapped to multiple loci" out/star/$sid/Log.final.out >> log/pipeline.log
+    grep "% of reads mapped to too many loci" out/star/$sid/Log.final.out >> log/pipeline.log
+    echo "----------------------------------------" >> log/pipeline.log
 done 
 
 # TODO: create a log file containing information from cutadapt and star logs
